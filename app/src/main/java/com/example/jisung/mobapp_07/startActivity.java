73e8f06875823563c2058afa114ce84c089df3eb
jisung <jisung0920@gmail.com>
2017-04-13 13:11:36 +0900

    init setting

--- app/src/main/java/com/example/jisung/mobapp_07/startActivity.java
+++ app/src/main/java/com/example/jisung/mobapp_07/startActivity.java
@@ -1,7 +1,9 @@
 package com.example.jisung.mobapp_07;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class startActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class startActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+    public void S_onClick(View view){
+        Intent intent;
+        if(view.getId()==R.id.s1) {
+            intent = new Intent(startActivity.this,PracActivity.class);
+        }
+        else
+            intent = new Intent(startActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
